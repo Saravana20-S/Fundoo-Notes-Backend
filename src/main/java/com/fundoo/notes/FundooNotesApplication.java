9a2cd9f6c71601c48c90dd9807d5eebe53289ef6
Saravana20-S <iamsaravanan1347@gmail.com>
2026-09-04 10:48:15 +0530

    Main file of fundoo logic added

---
+++ src/main/java/com/fundoo/notes/FundooNotesApplication.java
@@ -0,0 +1,14 @@
+package com.fundoo.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class FundooNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundooNotesApplication.class, args);
+	}
+}	
